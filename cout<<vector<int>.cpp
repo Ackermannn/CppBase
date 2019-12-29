268694dc20d367acb16a5d217dfc77b71655a1ec
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2019-12-29 14:35:32 +0800

    Create cout<<vector<int>.cpp

---
+++ cout<<vector<int>.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+/*
+* 重构cout 也接受vector<int>类型数据输入
+* 例子的输出效果为 [0,1,0,3,12] 
+*/
+using std::ostream;
+using std::vector;
+using std::endl;
+ostream& operator<<(ostream &os, vector<int>& v) 
+{
+	os << '[';
+	for (auto iter = v.begin(); iter < v.end() - 1; iter++) 
+	{
+		os << *iter << ',';
+	}
+	os << *(v.end()-1) << ']';
+	return os;
+}
+
+int main() {
+	vector<int> nums = { 0,1,0,3,12 };
+	cout << nums << endl;
+	return 0;
+}
