71035fbcb2371056163482370028435f64124480
夏尚梓 <49069545+Ackermannn@users.noreply.github.com>
2020-09-02 21:24:13 +0800

    Create main.cpp

---
+++ toys/汉语编程/main.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define 包含 include
+#define 打印 printf
+#define 返回 return
+#define 主程序 main
+#define 整型 int
+#define 装载 include
+#define 零 0
+
+整型 主程序() {
+
+	打印("问天地好在");
+
+	返回 零;
+}
